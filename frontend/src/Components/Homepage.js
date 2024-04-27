124104bbc7d5afb3d41b3e5570beeac76e435912
Phillip Ly <lyp@flip4.engr.oregonstate.edu>
2024-04-26 22:22:45 -0700

    fixed homepage

--- frontend/src/Components/Homepage.js
+++ frontend/src/Components/Homepage.js
@@ -14,31 +14,13 @@ function Homepage() {
             <Col md={7}>
               <p className="homepage-introduction">
                 <h1>About</h1>
-                Oregon State University's Database Tracker system records
-                student information for 400 current post baccalaureate students
-                enrolled in our program.
+                The Fitness Tracker application allows the user 
+                to add and read various exercises and foods logged. One can also view and track their
+                weight progress, login, and view profile information.
               </p>
             </Col>
           </Row>
-          <Row>
-            <Col md={7}>
-              <p
-                className="homepage-introduction"
-                style={{ marginBottom: "500px" }}
-              >
-                Please use the navigation links above to navigate through
-                different pages. The user can add, update, or delete students.
-                One can also add course, instructor and company information.
-                Users can also report reviews they've had for courses they have
-                taken. The user can also view information about a students
-                earnings, and see how OSU's program has impacted their salary.{" "}
-                <b>
-                  Included helpful tooltips below for certain fields
-                  that were unclear
-                </b>
-              </p>
-            </Col>
-          </Row>
+         
         </Container>
       </Container>
     </section>
